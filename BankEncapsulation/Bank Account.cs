181fbf6ca6c6539a56ec3817daf7640dee766885
jfranco9202 <jeremy.franco41@yahoo.com>
2022-01-13 21:26:32 -0600

    intial commit

---
+++ BankEncapsulation/Bank Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class Bank_Account
+
+
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount);
+        {
+            Console.WriteLine($"Simulating a deposit of {amount,0:c} to your account!!!" );
+            
+        }
+
+       public double GetBalance();
+    
+        {
+            return: _balance;
+        }
+
+    }
+
+
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank_Account ba = new Bank_Account();
+            
+            Console.WriteLine($"How much money would you like to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+            
+            ba.Deposit(amountToDeposit);
+            double UserBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balance is {UserBalance,0:C}");
+
+
+        }
+    }
+}
